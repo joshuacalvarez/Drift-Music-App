080c320fb9e2b58bfa2685ca5173826ae422cdf9
LumpyDood <isaacjoshuajordan@gmail.com>
2025-10-30 16:50:46 -0700

    fix config

--- next.config.ts
+++ next.config.ts
@@ -4,20 +4,21 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+
 module.exports = {
   async headers() {
     return [
       {
         source: '/api/:path*',
         headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          }
-        ]
-      }
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,PUT,DELETE,OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
+        ],
+      },
     ];
-  }
+  },
 };
+
 
 export default nextConfig;
