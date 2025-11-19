47c96385552ca1b2c8c030c4cf14cd6d416f24e1
LumpyDood <isaacjoshuajordan@gmail.com>
2025-11-19 13:33:16 -0700

    test

--- src/components/AlbumList.tsx
+++ src/components/AlbumList.tsx
@@ -1,36 +1,36 @@
-import React from 'react';
-import { Album } from '@/lib/types';
-import AlbumCard from './AlbumCard';
+// import React from 'react';
+// import { Album } from '@/lib/types';
+// import AlbumCard from './AlbumCard';
 
-interface AlbumListProps {
-    albumList: Album[];
-    onClick: (albumId: number, uri: string) => void;
-}
+// interface AlbumListProps {
+//     albumList: Album[];
+//     onClick: (albumId: number, uri: string) => void;
+// }
 
 
-const AlbumList = ({ albumList, onClick}: AlbumListProps) => {
+// const AlbumList = ({ albumList, onClick}: AlbumListProps) => {
     
-    // const handleSelectionOne = (albumId, uri) => {
-    //     console.log('Selected ID is ' + albumId);
-    //     props.onClick(albumId, uri);
-    // };
+//     // const handleSelectionOne = (albumId, uri) => {
+//     //     console.log('Selected ID is ' + albumId);
+//     //     props.onClick(albumId, uri);
+//     // };
 
-    console.log('props albumList', albumList);
-    const albums = albumList.map((album) => {
-        return (
-            <AlbumCard key={album.id} album={album} onClick={(albumId: number, uri: string) => onClick(albumId, uri)}/>
-        );
-    });
+//     console.log('props albumList', albumList);
+//     const albums = albumList.map((album) => {
+//         return (
+//             <AlbumCard key={album.id} album={album} onClick={(albumId: number, uri: string) => onClick(albumId, uri)}/>
+//         );
+//     });
 
 
-    return (
-    <div className="container">
-        <div className="d-flex flex-wrap">
-            {albums}
-        </div>
-    </div>
-    );
+//     return (
+//     <div className="container">
+//         <div className="d-flex flex-wrap">
+//             {albums}
+//         </div>
+//     </div>
+//     );
 
-};
+// };
 
-export default AlbumList;
+// export default AlbumList;
