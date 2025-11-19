f7f82a5839dd3dac05058dd45be58ea0cdca5225
LumpyDood <isaacjoshuajordan@gmail.com>
2025-11-19 13:47:27 -0700

    surely

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,12 @@
-"use client";
+'use client';
 
-import Link from 'next/link';
-import {useEffect} from 'react';
+import Link from "next/link";
+import { useEffect } from "react";
 
-export default function NavBar(){
-    useEffect(() => {
-        import('bootstrap/dist/js/bootstrap.bundle.min.js');
-    }, []);
+export default function NavBar() {
 
     return (
-        <nav className="navbar navbar-expand lg navbar-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <span className="navbar-brand">My Music</span>
             <button
                 className="navbar-toggler"
@@ -19,18 +16,20 @@ export default function NavBar(){
                 aria-controls="navbarNavAltMarkup"
                 aria-expanded="false"
                 aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav">
-                        <Link href="/" className="nav-item nav-link">
-                        Main</Link>
-                        <Link href="/new" className="nav-item nav-link">
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                    <Link href="/" className="nav-item nav-link">
+                        Main
+                    </Link>
+                    <Link href="/new" className="nav-item nav-link">
                         New
-                        </Link>
-                    </div>
+                    </Link>
                 </div>
+            </div>
         </nav>
-    )
+    );
+
 }
